57309bb2407fd03567a33418998cfdadb10f2897
Jack Ceroni <jackceroni@gmail.com>
2020-05-08 23:46:05 -0400

    coprime states

--- coprime_states/state_generation.py
+++ coprime_states/state_generation.py
@@ -1,5 +1,8 @@
 import numpy as np
+import scipy.linalg
 import math
+import random
+from matplotlib import pyplot as plt
 
 # Generating prime states numerically, for a given dimension
 
@@ -18,10 +21,56 @@ def Generate_Prime_State(dimension):
                 vector1[i-1] = 1
                 vector2[j-1] = 1
                 
-                final_vector += np.kron(vector2, vector1)
+                final_vector += np.kron(vector1, vector2)
             
     final_vector = final_vector / np.sqrt(sum(final_vector))
     
     return final_vector
 
-print(Generate_Prime_State(4))
+print(Generate_Prime_State(3))
+
+# Calculates the partial trace of the matrix
+
+def density_matrix(vector):
+
+    return np.outer(vector, np.conj(vector))
+
+def partial_trace(matrix):
+    
+    number = int(np.sqrt(len(matrix)))
+    new_matrix = []
+    
+    for i in range(0, number):
+        new_row = []
+        for j in range(0, number):
+            
+            sub = matrix[i*number:(i+1)*number, j*number:(j+1)*number]
+            new_row.append(np.trace(sub))
+        
+        new_matrix.append(new_row)
+    
+    return new_matrix
+            
+m = np.array([[1, 1, 2, 1], [1, 1, 1, 1], [4, 1, 1, 1], [1, 1, 1, 1]])
+print(partial_trace(m))
+
+# Finds the Von Neumann entropy
+
+def entropy(matrix):
+    
+    log = scipy.linalg.logm(matrix)
+    final_val = -1*np.trace(np.matmul(matrix, log))
+    return final_val
+
+dimensions = range(2, 50)
+y = []
+for i in dimensions:
+    
+    prime_state = Generate_Prime_State(i)
+    dm = density_matrix(prime_state)
+    new_dm = partial_trace(dm)
+    
+    y.append(entropy(new_dm))
+
+plt.plot(dimensions, y)
+plt.show()
